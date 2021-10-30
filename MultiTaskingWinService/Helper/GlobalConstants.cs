fa57656d0a29369b4e87f3a68cd44914d1d56864
DESKTOP-TFRDGC0\Jogendra Kumar <kumarjogendra0@gmail.com>
2021-10-30 08:25:20 +0530

    Add project files.

---
+++ MultiTaskingWinService/Helper/GlobalConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiTaskingWinService.Helper
+{
+    public enum ServiceInterval
+    {
+        OneMinuteInterval = 1,
+        TwoMinuteInterval = 2,
+        TwentyFourHours = 24,
+        OneHour = 1
+    }
+}
---
+++ MultiTaskingWinService/Helpers/CommonConstant.cs
@@ -0,0 +1,31 @@
+﻿namespace C9ISM.Scheduler.Helpers
+{
+    public static class CommonConstant
+    {
+        public static readonly string BaseUrl = "https://www.moneycontrol.com";
+
+        public static readonly string BlockDealNse = BaseUrl + "/stocks/marketstats/block-deals/nse/";
+        public static readonly string BlockDealBse = BaseUrl + "/stocks/marketstats/block-deals/bse/";
+        public static readonly string BulkDealNse = BaseUrl + "/stocks/marketstats/bulk-deals/nse/";
+        public static readonly string BulkDealBse = BaseUrl + "/stocks/marketstats/bulk-deals/bse/";
+        public static readonly string BulkBlockDealXpath = "/html/body/section/section/section/div/div[2]/div[2]/table/tbody/tr";
+
+        public static readonly string BaseApiUrl = "https://api.moneycontrol.com";
+        public static readonly string SMAPILink = BaseApiUrl + "/mcapi/technicals/v2/details?scId=CompanyCode&dur=D&deviceType=W";
+        public static readonly string VolDeliveryAPILink = BaseApiUrl + "/mcapi/v1/stock/price-volume?scId=CompanyCode";
+
+        public static readonly string BaseScreenUrl = "https://www.screener.in";
+        public static readonly string ScreenApi = BaseScreenUrl + "/api/company/search/?q=cname&v=3";
+
+        public static readonly string BlockHistoryBse = "https://www.bseindia.com/" + "download/BhavCopy/Equity/EQstrdate_CSV.ZIP";
+        public static readonly string BlockHistoryNse = "https://www1.nseindia.com/" + "content/historical/EQUITIES/year/month/cmstrdatebhav.csv.zip";
+        public static readonly string ZipPath = @"D:\Upwork\StockiHstory\";
+
+        //public static readonly string DBConnectionString = @"Data Source=DESKTOP-TFRDGC0\SQLExpress;Initial Catalog=StockMonitor;Integrated Security=True";
+
+        //public static readonly string BulkDealQuery = "INSERT INTO [dbo].[BlockBulkDeals] " +
+        //    "([company],[client],[transType],[quantity],[price_Traded] ,[price_Closed],[Type] ,[trnasactionDate],[Exchange])" +
+        //    "VALUES(@CompanyName,@Client,@TransactionType,@Quantity,@TradedPrice,@ClosedPrice,@TransactionType,@TransactionDate,@Exchange)";
+
+    }
+}
